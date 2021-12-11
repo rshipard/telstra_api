a9ed2b5c78292674a9f711e9857a754f8f90928b
Ralph <rshipard@gmail.com>
2021-12-12 10:39:04 +1100

    GET and POST to cars table working

--- server.js
+++ server.js
@@ -19,6 +19,29 @@ app.get('/', (req, res) => {
     res.send(`I'm listening on port ${port}`)
 })
 
+// Post a new car to DB
+app.post('/cars', async (req, res) => {
+    try {
+        const { registration_number, year, color, manufacturer, model } = req.body
+        const newCar = await pool.query("INSERT INTO cars (registration_number, year, color, manufacturer, model) VALUES ($1, $2, $3, $4, $5) RETURNING *", [registration_number.toUpperCase() , parseInt(year), color, manufacturer, model])
+        res.json(newCar.rows)
+    } catch (error) {
+        console.log(error.message)
+        throw error
+    }
+})
+
+// Return all cars
+app.get('/cars', async (req, res) => {
+    try {
+        const allCars = await pool.query("SELECT * FROM cars;")
+        res.json(allCars.rows)
+    } catch (error) {
+        console.log(error.message)
+        throw error
+    }
+})
+
 // testing get request to db
 app.get('/test', async (req, res) => {
     const results = await pool.query("SELECT * FROM testing")
